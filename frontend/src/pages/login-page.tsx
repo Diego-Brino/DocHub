cef606a3d7fcdfeca0b4bfcba1551ee006175947
Diego Manucci Bizzotto <diegobizzotto2003@gmail.com>
2024-03-31 19:30:29 -0300

    Refactor: Alterar mensagem de validação de email

--- frontend/src/pages/login-page.tsx
+++ frontend/src/pages/login-page.tsx
@@ -13,7 +13,7 @@ import {z} from "zod";
 const schema = z.object({
   email: z.string()
     .min(1, "Email é obrigatório!")
-    .email("Email inválido"),
+    .email("Email inválido!"),
   password: z.string()
     .min(1, "Senha é obrigatória!"),
 })
