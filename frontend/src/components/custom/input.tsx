d467e0bc0473e0354efb79fa67a40a10c05c2856
Diego Manucci Bizzotto <diegobizzotto2003@gmail.com>
2024-04-01 23:17:49 -0300

    Feat: Criar novos props de endIcon e onClickEndIcon

--- frontend/src/components/custom/input.tsx
+++ frontend/src/components/custom/input.tsx
@@ -1,27 +1,35 @@
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
+import {cn} from "@/lib/utils"
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  endIcon?: React.ReactNode,
+  onClickEndIcon?(event: React.MouseEvent<HTMLDivElement, MouseEvent> | undefined): void,
   error?: string
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, error, ...props }, ref) => {
+  ({ className, type, error, endIcon, onClickEndIcon, ...props }, ref) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className,
-          error && "border-red-500"
+      <div className={cn('relative border border-input rounded-md placeholder:text-muted-foreground focus-within:outline-none focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2', error && "border-red-500",)}>
+        <input
+          type={type}
+          className={cn("flex h-10 w-full rounded-md bg-background px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 outline-none", className, endIcon && "pr-[60px]")}
+          ref={ref}
+          {...props}
+        />
+        {endIcon && (
+          <div
+            className={cn('absolute right-0 -translate-y-full h-full flex justify-center items-center px-3 py-2 bg-primary rounded-r-[5px] text-white', props.disabled && 'cursor-not-allowed opacity-50', endIcon && onClickEndIcon && 'cursor-pointer hover:bg-primary/90')}
+            onClick={(event) => onClickEndIcon && onClickEndIcon(event)}
+          >
+            {endIcon}
+          </div>
         )}
-        ref={ref}
-        {...props}
-      />
+      </div>
     )
   }
 )
 Input.displayName = "Input"
 
-export { Input }
+export {Input}
