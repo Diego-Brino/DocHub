762809397c6011e461fe84afce41bbb927ddd721
Diego Manucci Bizzotto <diegobizzotto2003@gmail.com>
2024-04-07 19:56:51 -0300

    Refactor: Alterar método de criação de rotas para utilizar componente Routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {RouterProvider} from "react-router-dom";
-import router from "@/router/router.tsx";
+import {BrowserRouter} from "react-router-dom";
+import Router from "@/router/router.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <BrowserRouter>
+      <Router/>
+    </BrowserRouter>
   </React.StrictMode>,
 )
--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,23 +1,19 @@
-import {createBrowserRouter} from "react-router-dom";
+import {Route, Routes, useLocation} from "react-router-dom";
 import App from "../App.tsx";
 import {LoginPage} from "../pages/login/login-page.tsx";
 import {HomeLayout} from "@/layouts/home-layout.tsx";
 
-const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App/>,
-      children: [
-        {path: "login", element: <LoginPage/>},
-        {
-          path: "home",
-          element: <HomeLayout/>,
-          children: [
+const Router = () => {
+  const location = useLocation();
 
-          ]
-        },
-      ]
-    }
-])
+  return (
+    <Routes location={location} key={location.pathname}>
+      <Route path="/" element={<App/>}>
+        <Route path="login" element={<LoginPage/>}/>
+        <Route path="home/*" element={<HomeLayout/>}/>
+      </Route>
+    </Routes>
+  );
+}
 
-export default router;
+export default Router;
