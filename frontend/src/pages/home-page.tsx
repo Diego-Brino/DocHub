fd4e7903f35fec58a01b4dc6cacf25298cfeecf2
Diego Manucci Bizzotto <diegobizzotto2003@gmail.com>
2024-04-21 20:02:20 -0300

    Refactor: Mover páginas para raiz da pasta pages

---
+++ frontend/src/pages/home-page.tsx
@@ -0,0 +1,13 @@
+import {Dashboard} from "@/layouts/dashboard";
+
+function HomePage() {
+  return (
+    <Dashboard>
+      <p>HOME</p>
+    </Dashboard>
+  )
+}
+
+HomePage.displayName = 'HomePage';
+
+export { HomePage };
