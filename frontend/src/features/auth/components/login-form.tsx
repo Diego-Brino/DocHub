2892125e3e01ee1d347f1586d6439c9f12b726b9
Diego Manucci Bizzotto <diegobizzotto2003@gmail.com>
2024-04-28 12:44:32 -0300

    Refactor: Transformar formulário em componente

---
+++ frontend/src/features/auth/components/login-form.tsx
@@ -0,0 +1,80 @@
+import {CardContent, CardFooter} from "@/components/ui/card.tsx";
+import {Label} from "@/components/ui/label.tsx";
+import {EmailInput, Login, PasswordInput} from "@/features/auth";
+import {InputValidation} from "@/components/custom/input-validation.tsx";
+import {Button} from "@/components/custom/button.tsx";
+import {z} from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+
+const schema = z.object({
+  email: z.string()
+    .min(1, "Email é obrigatório!")
+    .email("Email inválido!"),
+  password: z.string()
+    .min(1, "Senha é obrigatória!"),
+})
+
+function LoginForm() {
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false)
+
+  const {
+    register,
+    handleSubmit,
+    formState: {errors}
+  } = useForm<Login>({
+    resolver: zodResolver(schema),
+  })
+
+  const onSubmit = (data: Login) => {
+    console.log(data);
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      navigate('/home')
+    }, 2000)
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <CardContent className='space-y-4'>
+        <div className='space-y-2'>
+          <Label htmlFor='email'>Email</Label>
+          <EmailInput
+            {...register('email')}
+            id='email'
+            error={errors?.email?.toString()}
+          />
+          <InputValidation error={errors?.email?.message?.toString()}/>
+        </div>
+        <div className='space-y-2'>
+          <div className="flex items-center justify-between gap-2">
+            <Label htmlFor="password">Senha</Label>
+            <Button variant='link' asChild className='p-0 h-min underline text-current hover:text-primary'>
+              <a href='#'>Esqueceu a senha?</a>
+            </Button>
+          </div>
+          <PasswordInput
+            {...register('password')}
+            id='password'
+            error={errors?.password?.toString()}
+          />
+          <InputValidation error={errors?.password?.message?.toString()}/>
+        </div>
+      </CardContent>
+      <CardFooter className='block space-y-2'>
+        <Button type='submit' loading={loading} disabled={loading} className='w-full'>
+          Entrar
+        </Button>
+      </CardFooter>
+    </form>
+  )
+}
+
+LoginForm.displayName = "LoginForm"
+
+export {LoginForm};
--- frontend/src/pages/login-page.tsx
+++ frontend/src/pages/login-page.tsx
@@ -1,46 +1,9 @@
-import {useState} from "react";
-import {useForm} from "react-hook-form";
-import {zodResolver} from "@hookform/resolvers/zod";
-import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card.tsx";
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card.tsx";
 import logo from "@/assets/logo.svg";
 import {Separator} from "@/components/ui/separator.tsx";
-import {Label} from "@/components/ui/label.tsx";
-import {InputValidation} from "@/components/custom/input-validation.tsx";
-import {Button} from "@/components/custom/button.tsx";
-import {z} from "zod";
-import {useNavigate} from "react-router-dom";
-import {PasswordInput, EmailInput, Login} from "@/features/auth";
-
-const schema = z.object({
-  email: z.string()
-    .min(1, "Email é obrigatório!")
-    .email("Email inválido!"),
-  password: z.string()
-    .min(1, "Senha é obrigatória!"),
-})
+import {LoginForm} from "@/features/auth/components/login-form.tsx";
 
 function LoginPage() {
-  const navigate = useNavigate();
-
-  const {
-    register,
-    handleSubmit,
-    formState: {errors}
-  } = useForm<Login>({
-    resolver: zodResolver(schema),
-  })
-
-  const [loading, setLoading] = useState(false)
-
-  const onSubmit = (data: Login) => {
-    console.log(data);
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-      navigate('/home')
-    }, 2000)
-  }
-
   return (
     <div className='container h-screen flex flex-col gap-6 justify-center items-center'>
       <div className='flex md:hidden gap-4 justify-center'>
@@ -73,38 +36,7 @@ function LoginPage() {
                 </div>
               </CardTitle>
             </CardHeader>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <CardContent className='space-y-4'>
-                <div className='space-y-2'>
-                  <Label htmlFor='email'>Email</Label>
-                  <EmailInput
-                    {...register('email')}
-                    id='email'
-                    error={errors?.email?.toString()}
-                  />
-                  <InputValidation error={errors?.email?.message?.toString()}/>
-                </div>
-                <div className='space-y-2'>
-                  <div className="flex items-center justify-between gap-2">
-                    <Label htmlFor="password">Senha</Label>
-                    <Button variant='link' asChild className='p-0 h-min underline text-current hover:text-primary'>
-                      <a href='#'>Esqueceu a senha?</a>
-                    </Button>
-                  </div>
-                  <PasswordInput
-                    {...register('password')}
-                    id='password'
-                    error={errors?.password?.toString()}
-                  />
-                  <InputValidation error={errors?.password?.message?.toString()}/>
-                </div>
-              </CardContent>
-              <CardFooter className='block space-y-2'>
-                <Button type='submit' loading={loading} disabled={loading} className='w-full'>
-                  Entrar
-                </Button>
-              </CardFooter>
-            </form>
+            <LoginForm/>
           </div>
         </div>
       </Card>
