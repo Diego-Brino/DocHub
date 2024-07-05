d41de851c0ccbe6bcc220d05fd2917852818f618
dbizzotto <dbizzotto@unaerp.br>
2024-07-05 15:39:30 -0300

    Refactor: Remover AnimatePresence das rotas

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,7 +1,6 @@
 import {Route, Routes, useLocation} from "react-router-dom";
 import App from "../App.tsx";
 import {LoginPage} from "../pages/login-page.tsx";
-import {AnimatePresence} from "framer-motion";
 import {HomePage} from "@/pages/home-page.tsx";
 import {FilesPage} from "@/pages/files-page.tsx";
 import {GroupsPage} from "@/pages/groups-page.tsx";
@@ -16,22 +15,20 @@ const Router = () => {
   const location = useLocation();
 
   return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="" element={<App/>}>
-          <Route path="login" element={<LoginPage/>}/>
-          <Route path="home" element={<HomePage/>}/>
-          <Route path="files" element={<FilesPage/>}/>
-          <Route path="groups" element={<GroupsPage/>}/>
-          <Route path="flows" element={<FlowsPage/>}/>
-          <Route path="permissions" element={<PermissionsPage/>}/>
-          <Route path="monitoring" element={<MonitoringPage/>}/>
-          <Route path="profile" element={<ProfilePage/>}/>
-          <Route path="users" element={<UsersPage/>}/>
-          <Route path="settings" element={<SettingsPage/>}/>
-        </Route>
-      </Routes>
-    </AnimatePresence>
+    <Routes location={location} key={location.pathname}>
+      <Route path="" element={<App/>}>
+        <Route path="login" element={<LoginPage/>}/>
+        <Route path="home" element={<HomePage/>}/>
+        <Route path="files" element={<FilesPage/>}/>
+        <Route path="groups" element={<GroupsPage/>}/>
+        <Route path="flows" element={<FlowsPage/>}/>
+        <Route path="permissions" element={<PermissionsPage/>}/>
+        <Route path="monitoring" element={<MonitoringPage/>}/>
+        <Route path="profile" element={<ProfilePage/>}/>
+        <Route path="users" element={<UsersPage/>}/>
+        <Route path="settings" element={<SettingsPage/>}/>
+      </Route>
+    </Routes>
   );
 }
 
