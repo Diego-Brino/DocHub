ad80dcf8877e965016dd0113956243d9a9051b3f
diego <diegobizzotto2003@gmail.com>
2024-07-06 09:10:16 -0300

    Feat: Adicionar rota vazia para tela de login

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,15 +1,7 @@
 import {Route, Routes, useLocation} from "react-router-dom";
 import App from "../App.tsx";
 import {LoginPage} from "../pages/login-page.tsx";
-import {HomePage} from "@/pages/home-page.tsx";
-import {FilesPage} from "@/pages/files-page.tsx";
 import {GroupsPage} from "@/pages/groups-page.tsx";
-import {FlowsPage} from "@/pages/flows-page.tsx";
-import {PermissionsPage} from "@/pages/permissions-page.tsx";
-import {MonitoringPage} from "@/pages/monitoring-page.tsx";
-import {ProfilePage} from "@/pages/profile-page.tsx";
-import {UsersPage} from "@/pages/users-page.tsx";
-import {SettingsPage} from "@/pages/settings-page.tsx";
 import {NotFoundPage} from "@/pages/not-found-page.tsx";
 
 const Router = () => {
@@ -17,17 +9,12 @@ const Router = () => {
 
   return (
     <Routes location={location} key={location.pathname}>
-      <Route path="" element={<App/>}>
+      <Route path="/" element={<App/>}>
+        {["/", "login"].map(path => (
+            <Route path={path} element={<LoginPage/>}/>
+        ))}
         <Route path="login" element={<LoginPage/>}/>
-        <Route path="home" element={<HomePage/>}/>
-        <Route path="files" element={<FilesPage/>}/>
         <Route path="groups" element={<GroupsPage/>}/>
-        <Route path="flows" element={<FlowsPage/>}/>
-        <Route path="permissions" element={<PermissionsPage/>}/>
-        <Route path="monitoring" element={<MonitoringPage/>}/>
-        <Route path="profile" element={<ProfilePage/>}/>
-        <Route path="users" element={<UsersPage/>}/>
-        <Route path="settings" element={<SettingsPage/>}/>
         <Route path='*' element={<NotFoundPage/>} />
       </Route>
     </Routes>
