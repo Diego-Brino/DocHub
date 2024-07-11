c5eb9c4b66beac9c876687a0468f456b65333652
diego <diegobizzotto2003@gmail.com>
2024-07-11 00:56:53 -0300

    Feat: Organizar disposição do conteúdo no layout

--- frontend/src/layouts/main/components/header.tsx
+++ frontend/src/layouts/main/components/header.tsx
@@ -4,7 +4,7 @@ import {HeaderLinks} from "@/layouts/main/components/header-links.tsx";
 
 function Header() {
   return (
-    <div className='flex justify-between p-4 gap-4 bg-background border-b'>
+    <div className='flex justify-between p-4 gap-4 bg-background border-b bg-opacity-60 backdrop-blur-xl'>
       <HeaderLogo/>
       <HeaderLinks/>
       <HeaderProfile/>
--- frontend/src/layouts/main/components/main.tsx
+++ frontend/src/layouts/main/components/main.tsx
@@ -3,9 +3,11 @@ import {Header} from "./header.tsx";
 
 function Main() {
   return (
-    <div className='grid grid-cols-1 grid-rows-2'>
+    <div className='grid grid-cols-1 grid-rows-[max-content_1fr] min-h-screen'>
       <Header/>
-      <Outlet/>
+      <div className='flex bg-background-60 md:m-8 p-8 rounded md:border backdrop-blur-xl'>
+        <Outlet/>
+      </div>
     </div>
   )
 }
