3c07b3d1eb0c742a58a016829b06d5a0f57dc3b1
diego <diegobizzotto2003@gmail.com>
2024-07-12 00:26:00 -0300

    Refactor: Definir cor do ícone do input para ser sempre white

--- frontend/src/components/custom/input.tsx
+++ frontend/src/components/custom/input.tsx
@@ -11,7 +11,7 @@ export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, error, endIcon, onClickEndIcon, ...props }, ref) => {
     return (
-      <div className={cn('relative border border-input rounded-md placeholder:text-muted-foreground focus-within:outline-none focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2', error && "border-red-500",)}>
+      <div className={cn('relative border border-input rounded-md placeholder:text-muted-foreground focus-within:outline-none focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2', error && "border-red-500", className)}>
         <input
           type={type}
           className={cn("flex h-10 w-full rounded-md bg-background px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 outline-none", className, endIcon && "pr-[60px]")}
@@ -20,7 +20,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         />
         {endIcon && (
           <div
-            className={cn('absolute right-0 -translate-y-full h-full flex justify-center items-center px-3 py-2 bg-primary rounded-r-[5px] text-secondary', props.disabled && 'cursor-not-allowed opacity-50', endIcon && onClickEndIcon && 'cursor-pointer hover:bg-primary/90')}
+            className={cn('absolute right-0 -translate-y-full h-full flex justify-center items-center px-3 py-2 bg-primary rounded-r-[5px] text-white', props.disabled && 'cursor-not-allowed opacity-50', endIcon && onClickEndIcon && 'cursor-pointer hover:bg-primary/90')}
             onClick={(event) => onClickEndIcon && onClickEndIcon(event)}
           >
             {endIcon}
