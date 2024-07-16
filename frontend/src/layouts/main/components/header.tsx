79cb58b9bcd48ba72ff09785e21e61173e98be81
diego <diegobizzotto2003@gmail.com>
2024-07-15 22:41:26 -0300

    Refactor: Ajustar padding do header no mobile

--- frontend/src/layouts/main/components/header.tsx
+++ frontend/src/layouts/main/components/header.tsx
@@ -5,7 +5,7 @@ import {HeaderLinks} from "./header-links.tsx";
 function Header() {
   return (
     <div className='bg-muted/60 border-b h-[73px]'>
-      <div className='container flex justify-between py-4 gap-4'>
+      <div className='md:container flex justify-between p-4 gap-4'>
         <HeaderLogo/>
         <HeaderLinks/>
         <HeaderProfile/>
