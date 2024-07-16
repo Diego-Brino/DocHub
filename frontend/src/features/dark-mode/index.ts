4ec76c64ab41d791a2f3876ce524efdb8ade7002
diego <diegobizzotto2003@gmail.com>
2024-07-15 22:38:33 -0300

    Feat: Criar DarkModeButton

--- frontend/src/features/dark-mode/index.ts
+++ frontend/src/features/dark-mode/index.ts
@@ -1,7 +1,9 @@
 import {DarkModeProvider} from "@/features/dark-mode/providers/dark-mode-provider.tsx";
 import {DarkModeSwitch} from "@/features/dark-mode/components/dark-mode-switch.tsx";
+import {DarkModeButton} from "@/features/dark-mode/components/dark-mode-button.tsx";
 
 export {
   DarkModeProvider,
-  DarkModeSwitch
+  DarkModeSwitch,
+  DarkModeButton
 };
