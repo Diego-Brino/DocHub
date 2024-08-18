ce19ca4d48e7082d8fb641bf59cd15ad35389e2b
diego <diegobizzotto2003@gmail.com>
2024-08-18 16:01:17 -0300

    Refactor: Remover msw

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,17 +3,8 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from "@/app/App.tsx";
 
-async function setupMockWorker() {
-  const {worker} = await import("@/mocks/browser.ts");
-  return worker.start({
-    onUnhandledRequest: "bypass"
-  });
-}
-
-setupMockWorker().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App/>
-    </React.StrictMode>,
-  )
-})
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+)
--- frontend/src/mocks/browser.ts
+++
@@ -1,6 +0,0 @@
-import {setupWorker} from "msw/browser";
-import {handlers} from "@/mocks/handlers.ts";
-
-const worker = setupWorker(...handlers);
-
-export {worker}
