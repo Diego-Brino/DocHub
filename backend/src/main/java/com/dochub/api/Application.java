8d5cc2b01f4a5de983411c7ae1b3b7726e2746b7
dbizzotto <dbizzotto@unaerp.br>
2024-08-21 09:51:26 -0300

    Chore: Alterar método de importação de variáveis sensíveis no backend

--- backend/src/main/java/com/dochub/api/Application.java
+++ backend/src/main/java/com/dochub/api/Application.java
@@ -1,6 +1,5 @@
 package com.dochub.api;
 
-import com.dochub.api.infra.env.DotenvInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -10,8 +9,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(Application.class);
-
-		app.addInitializers(new DotenvInitializer());
 		app.run(args);
 	}
 }
