e137adad61b5913519ee94a8eaae17df2bf15e7f
diego <diegobizzotto2003@gmail.com>
2024-08-25 13:05:14 -0300

    Fix: Corrigir endIcon do input

--- frontend/src/components/custom/input.tsx
+++ frontend/src/components/custom/input.tsx
@@ -1,10 +1,10 @@
 import * as React from "react"
-import {createElement} from "react"
+import {cloneElement} from "react"
 
 import {cn} from "@/lib/utils"
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  endIcon?: React.ReactNode,
+  endIcon?: React.ReactElement,
   onClickEndIcon?(event: React.MouseEvent<HTMLDivElement, MouseEvent>): void,
   error?: string
 }
@@ -24,7 +24,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             className={cn('absolute right-0 -translate-y-full h-full flex justify-center items-center px-3 py-2 bg-primary rounded-r-[5px] text-secondary', props.disabled && 'cursor-not-allowed opacity-50', endIcon && onClickEndIcon && 'cursor-pointer hover:bg-primary/90')}
             onClick={(event) => onClickEndIcon && onClickEndIcon(event)}
           >
-            {typeof endIcon === 'function' ? createElement(endIcon, { className: 'size-5' }) : endIcon}
+            {endIcon && cloneElement(endIcon, { className: cn('size-5', endIcon.props.className) })}
           </div>
         )}
       </div>
