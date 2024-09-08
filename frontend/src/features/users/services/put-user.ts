6e40fba4e4dd4628fdda22f33900b4e5bf9cea23
diego <diegobizzotto2003@gmail.com>
2024-09-08 15:31:26 -0300

    Fix: Enviar formData ao invés de json no putUser

--- frontend/src/features/users/services/put-user.ts
+++ frontend/src/features/users/services/put-user.ts
@@ -7,7 +7,13 @@ export type PutUserRequest = {
 }
 
 async function putUser(token: string, id: number, user: PutUserRequest): Promise<void> {
-  await axiosClient.put(`/users/${id}`, user, {
+  const formData = new FormData();
+
+  formData.append('name', user.name);
+  formData.append('email', user.email);
+  formData.append('username', user.username);
+
+  await axiosClient.put(`/users/${id}`, formData, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
