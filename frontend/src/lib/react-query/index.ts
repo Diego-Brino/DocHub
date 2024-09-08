1504da69d50b1114530c197daa3f3c51794d095d
diego <diegobizzotto2003@gmail.com>
2024-09-08 13:59:48 -0300

    Refactor: Remover retry do toast

--- frontend/src/lib/react-query/index.ts
+++ frontend/src/lib/react-query/index.ts
@@ -1,8 +1,6 @@
 import {MutationCache, QueryCache, QueryClient} from "react-query";
 import axios from "axios";
 import {toast} from "sonner";
-import {RotateCcw} from "lucide-react";
-import {createElement} from "react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -11,38 +9,22 @@ const queryClient = new QueryClient({
     }
   },
   queryCache: new QueryCache({
-    onError: ((error , query) => {
+    onError: ((error) => {
       if(!axios.isAxiosError(error)) {
         return;
       }
-
       toast.error('Erro', {
-        description: error?.response?.data.message || error.message,
-        classNames: {
-          actionButton: '!inline-flex !items-center !justify-center !whitespace-nowrap !rounded-md !text-sm !font-medium !ring-offset-background !transition-colors !focus-visible:!outline-none !focus-visible:!ring-2 !focus-visible:!ring-ring !focus-visible:!ring-offset-2 !disabled:!pointer-events-none !disabled:!opacity-50 !border !border-input !bg-primary !hover:bg-primary/90 text-primary-foreground !h-10 !w-10'
-        },
-        action: {
-          label: createElement(RotateCcw, { className: 'size-5' }),
-          onClick: () => query.fetch()
-        }
+        description: error?.response?.data.message || error.message
       })
     })
   }),
   mutationCache: new MutationCache({
-    onError: ((error , _variables, _context, mutation) => {
+    onError: ((error) => {
       if(!axios.isAxiosError(error)) {
         return;
       }
-
       toast.error('Erro', {
-        description: error?.response?.data.message || error.message,
-        classNames: {
-          actionButton: '!inline-flex !items-center !justify-center !whitespace-nowrap !rounded-md !text-sm !font-medium !ring-offset-background !transition-colors !focus-visible:!outline-none !focus-visible:!ring-2 !focus-visible:!ring-ring !focus-visible:!ring-offset-2 !disabled:!pointer-events-none !disabled:!opacity-50 !border !border-input !bg-primary !hover:bg-primary/90 text-primary-foreground !h-10 !w-10'
-        },
-        action: {
-          label: createElement(RotateCcw, { className: 'size-5' }),
-          onClick: () => mutation.execute()
-        }
+        description: error?.response?.data.message || error.message
       })
     })
   }),
