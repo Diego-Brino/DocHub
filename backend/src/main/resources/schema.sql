aa2500a399ee53da9ff0c8ec1a932b9d203abba3
diego <diegobizzotto2003@gmail.com>
2024-09-17 21:35:54 -0300

    Fix: Truncar tabelas para resolver problema de PK na hora de subir a api localmente

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,3 +1,33 @@
+
+SET foreign_key_checks = 0;
+
+TRUNCATE TABLE `dochub`.`resposta`;
+TRUNCATE TABLE `dochub`.`servico`;
+TRUNCATE TABLE `dochub`.`permissao_recurso`;
+TRUNCATE TABLE `dochub`.`permissao_sistema`;
+TRUNCATE TABLE `dochub`.`permissao_grupo`;
+TRUNCATE TABLE `dochub`.`atividade`;
+TRUNCATE TABLE `dochub`.`cargo`;
+TRUNCATE TABLE `dochub`.`grupo`;
+TRUNCATE TABLE `dochub`.`cargo_permissao_grupo`;
+TRUNCATE TABLE `dochub`.`cargo_permissao_sistema`;
+TRUNCATE TABLE `dochub`.`atividade`;
+TRUNCATE TABLE `dochub`.`usuario`;
+TRUNCATE TABLE `dochub`.`processo`;
+TRUNCATE TABLE `dochub`.`recurso`;
+TRUNCATE TABLE `dochub`.`usuario_cargo`;
+TRUNCATE TABLE `dochub`.`auditoria_recuperacao_senha`;
+TRUNCATE TABLE `dochub`.`cargo_permissao_recurso`;
+TRUNCATE TABLE `dochub`.`fluxo`;
+TRUNCATE TABLE `dochub`.`fluxo_resposta`;
+TRUNCATE TABLE `dochub`.`historico`;
+TRUNCATE TABLE `dochub`.`pasta`;
+TRUNCATE TABLE `dochub`.`usuario_cargo_fluxo`;
+TRUNCATE TABLE `dochub`.`arquivo`;
+TRUNCATE TABLE `dochub`.`processo_recurso`;
+
+SET foreign_key_checks = 1;
+
 -- dochub.atividade definition
 
 CREATE TABLE IF NOT EXISTS `dochub`.`atividade` (
