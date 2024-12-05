23372b2e1cfd0e197fe0a951a2882fbe275f3de9
Diego Brino <diego.simonaio@hotmail.com>
2024-12-05 00:30:28 -0300

    Feat: Recuperar permissões de Recurso

--- backend/src/main/java/com/dochub/api/repositories/ResourceRepository.java
+++ backend/src/main/java/com/dochub/api/repositories/ResourceRepository.java
@@ -1,10 +1,21 @@
 package com.dochub.api.repositories;
 
+import com.dochub.api.dtos.resource.ResourceRolePermissionDTO;
 import com.dochub.api.entities.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ResourceRepository extends JpaRepository<Resource, Integer> {
-
+    @Query("""
+        SELECT new com.dochub.api.dtos.resource.ResourceRolePermissionDTO(r, grr.role, grr.resourcePermission)
+           FROM Resource r
+        LEFT JOIN ResourceRolePermission grr ON ( grr.resource.id = r.id )
+        WHERE r.id = :resourceId
+    """)
+    Optional<List<ResourceRolePermissionDTO>> findResourceRolesPermissionsByResourceId(Integer resourceId);
 }
