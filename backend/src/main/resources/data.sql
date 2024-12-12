bac75589759a02c1fcad0b012e3ac808bf105c44
Diego Brino <diego.simonaio@hotmail.com>
2024-12-11 23:17:03 -0300

    Feat: Adicionar novo mapeamento de fluxo.

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -30,9 +30,9 @@ SET foreign_key_checks = 1;
 
 INSERT INTO usuario (NOME, SENHA, EMAIL, USERNAME, AVATAR, DATA_ULTIMO_ACESSO, USUARIO_INSERCAO, DATA_INSERCAO, USUARIO_ALTERACAO, DATA_ALTERACAO)
 VALUES
-    ('João Silva', '$2a$10$zSgELT6bMGCegYxsMrkITeUGUkanQfWA1TvX.8POzxaM1c54vx4jK', 'joaosilva@exemplo.com', 'joao_silva', NULL, NULL, 'DocHub', '2024-08-21 11:01:10', NULL, NULL),
-    ('Diego Manucci Bizzotto', '$2a$10$zSgELT6bMGCegYxsMrkITeUGUkanQfWA1TvX.8POzxaM1c54vx4jK', 'diego.bizzotto@sou.unaerp.edu.br', 'dbizzotto', NULL, NULL, 'DocHub', '2024-08-21 11:01:10', NULL, NULL),
-    ('Diego Simonaio Brino', '$2a$10$zSgELT6bMGCegYxsMrkITeUGUkanQfWA1TvX.8POzxaM1c54vx4jK', 'diego.brino@sou.unaerp.edu.br', 'dbrino', NULL, NULL, 'DocHub', '2024-08-21 11:01:10', NULL, NULL);
+    ('Danilo Aono Ribeiro', '$2a$10$zSgELT6bMGCegYxsMrkITeUGUkanQfWA1TvX.8POzxaM1c54vx4jK', 'daono@unaerp.br', 'daono', NULL, NULL, 'DocHub', CURRENT_DATE, NULL, NULL),
+    ('Diego Manucci Bizzotto', '$2a$10$zSgELT6bMGCegYxsMrkITeUGUkanQfWA1TvX.8POzxaM1c54vx4jK', 'diego.bizzotto@sou.unaerp.edu.br', 'dbizzotto', NULL, NULL, 'DocHub', CURRENT_DATE, NULL, NULL),
+    ('Diego Simonaio Brino', '$2a$10$zSgELT6bMGCegYxsMrkITeUGUkanQfWA1TvX.8POzxaM1c54vx4jK', 'diego.brino@sou.unaerp.edu.br', 'dbrino', NULL, NULL, 'DocHub', CURRENT_DATE, NULL, NULL);
 
 INSERT INTO permissao_sistema (DESCRICAO, USUARIO_INSERCAO, DATA_INSERCAO)
 VALUES
@@ -273,3 +273,46 @@ VALUES
     (2, 'planejamento_orcamentario.docx', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 35.8, 'DocHub', CURRENT_DATE),
     (3, 'organograma_equipe_rh.png', 'image/png', 2, 'DocHub', CURRENT_DATE),
     (4, 'manual_procedimentos_dp.pdf', 'application/pdf', 1, 'DocHub', CURRENT_DATE);
+
+INSERT INTO servico (DESCRICAO, USUARIO_INSERCAO, DATA_INSERCAO)
+VALUES
+    ('Comprar', 'DocHub', CURRENT_DATE);
+
+INSERT INTO processo (DATA_INICIO, ID_SERVICO, ID_GRUPO, USUARIO_INSERCAO, DATA_INSERCAO)
+VALUES
+    ('2024-12-12', 1, 1, 'DocHub', CURRENT_DATE);
+
+INSERT INTO atividade (DESCRICAO, USUARIO_INSERCAO, DATA_INSERCAO)
+VALUES
+    ('Submissão da Proposta de Compra', 'DocHub', CURRENT_DATE),
+    ('Verificação da Solicitação', 'DocHub', CURRENT_DATE),
+    ('Pagamento', 'DocHub', CURRENT_DATE);
+
+INSERT INTO resposta (DESCRICAO, USUARIO_INSERCAO, DATA_INSERCAO)
+VALUES
+    ('Submeter', 'DocHub', CURRENT_DATE),
+    ('Aprovar', 'DocHub', CURRENT_DATE),
+    ('Negar', 'DocHub', CURRENT_DATE),
+    ('Salvar Comprovantes', 'DocHub', CURRENT_DATE);
+
+INSERT INTO fluxo (ORDEM, PRAZO, ID_PROCESSO, ID_ATIVIDADE, USUARIO_INSERCAO, DATA_INSERCAO)
+VALUES
+    (1, 5, 1, 1, 'DocHub', CURRENT_DATE),
+    (2, 5, 1, 2, 'DocHub', CURRENT_DATE),
+    (3, 2, 1, 3, 'DocHub', CURRENT_DATE);
+
+INSERT INTO fluxo_resposta (ID_FLUXO, ID_RESPOSTA, ID_FLUXO_DESTINO, USUARIO_INSERCAO, DATA_INSERCAO)
+VALUES
+    (1, 1, 2, 'DocHub', CURRENT_DATE),
+    (2, 2, 3, 'DocHub', CURRENT_DATE),
+    (2, 3, NULL, 'DocHub', CURRENT_DATE),
+    (3, 4, NULL, 'DocHub', CURRENT_DATE);
+
+INSERT INTO usuario_fluxo (ID_USUARIO, ID_FLUXO, USUARIO_INSERCAO, DATA_INSERCAO)
+VALUES
+    (1, 1, 'DocHub', CURRENT_DATE),
+    (1, 2, 'DocHub', CURRENT_DATE),
+    (1, 3, 'DocHub', CURRENT_DATE),
+    (2, 1, 'DocHub', CURRENT_DATE),
+    (2, 2, 'DocHub', CURRENT_DATE),
+    (2, 3, 'DocHub', CURRENT_DATE);
